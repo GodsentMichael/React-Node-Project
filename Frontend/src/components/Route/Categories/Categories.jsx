0d8f8b92777d6570dc10f1c4ac482ca52e643a0f
Godsent Michael <gentlemotivator@gmail.com>
2023-09-26 18:27:52 +0100

    Started: Category under hero.

---
+++ Frontend/src/components/Route/Categories/Categories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../../../styles/styles'
+
+
+const Categories = () => {
+  return (
+    <>
+    <div className={`${styles.section}`}>
+    <div className={`branding my-12 flex justify-between w-full shadow-sm bg-white p-5 rounded-md`} >
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Categories
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -3,12 +3,15 @@ import Home from '../components/Home/Home';
 import Header from '../components/Layout/Header';
 import Navbar from '../components/Layout/Navbar';
 import Hero from '../components/Route/Hero/Hero.jsx'
+import Categories from '../components/Route/Categories/Categories'
+
 
 const HomePage = () => {
 	return (
 		<div>
 			<Header activeHeading={1}/>
 			<Hero  />
+			<Categories/>
 		</div>
 	);
 };
